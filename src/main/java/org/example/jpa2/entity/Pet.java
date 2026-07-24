745c089049ad87ad2dcfddcdeeab626308b1da45
a1l1ke <onetenthplease@gmail.com>
2026-07-24 09:43:01 +0900

    반려동물 엔티티 추가

---
+++ src/main/java/org/example/jpa2/entity/Pet.java
@@ -0,0 +1,24 @@
+package org.example.jpa2.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+// 애너테이션 (@)
+// JPA
+@Entity
+// Lombok
+@Getter
+@ToString
+// 생성자
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA가 사용 가능
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false, length = 20)
+    private String name;
+    @Column(nullable = false)
+    private int age;
+}
