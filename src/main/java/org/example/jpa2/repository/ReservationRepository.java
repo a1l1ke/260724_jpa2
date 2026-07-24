d224416cb97769a6839111f174699031a595d9f3
a1l1ke <onetenthplease@gmail.com>
2026-07-24 14:24:36 +0900

    수의사와 예약 JPA 저장소 추가

---
+++ src/main/java/org/example/jpa2/repository/ReservationRepository.java
@@ -0,0 +1,7 @@
+package org.example.jpa2.repository;
+
+import org.example.jpa2.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
