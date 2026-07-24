e77822d286fd5767c40217e4506421bb3deb5531
a1l1ke <onetenthplease@gmail.com>
2026-07-24 09:45:38 +0900

    반려동물 JPA 저장소 구현

--- src/main/java/org/example/jpa2/repository/PetRepository.java
+++ src/main/java/org/example/jpa2/repository/PetRepository.java
@@ -1,4 +1,11 @@
 package org.example.jpa2.repository;
 
-public class PetRepository {
+import org.example.jpa2.entity.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 1. interface -> extends Jpa...
+// 2. generic -> <Entity, Id>
+// 3. Jpa... -> NoBeanRepository -> Spring Container
+// 4. 기본적인 CRUD 모두 구현 -> 추가 기능들 Query Method, JPQL, QueryDSL...
+public interface PetRepository extends JpaRepository<Pet, Long> {
 }
